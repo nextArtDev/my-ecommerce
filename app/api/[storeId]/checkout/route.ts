c7ec78462ca9b58fe94036a3e047d618b8935a03
Saeed Mehmanparast <nextartisticdevelopments@gmail.com>
2023-09-14 22:57:24 +0330

    zarinpal first results with monopay

--- app/api/[storeId]/checkout/route.ts
+++ app/api/[storeId]/checkout/route.ts
@@ -1,15 +1,13 @@
 import { getAuthSession } from '@/lib/auth'
 import { prisma } from '@/lib/prisma'
-// import { zarinpal } from '@/lib/zarinpal'
-// import { ZarinpalType } from '@/types/zarinpal'
-// import Zarinpal from 'zarinpal-checkout-v4'
 import { NextResponse } from 'next/server'
-import { InitParams } from 'zarinpal-checkout-v4/lib/types/InitParams'
-// import ZarinpalPayment from 'zarinpal-pay'
-// // const zarinpal = new ZarinpalPayment(Merchant, isTomam, isSandbox)
 
-// import { PaymentMetadata, PaymentRequest } from 'zarinpal-checkout-v4/lib/types'
-import { getPaymentDriver } from 'monopay'
+import { getPaymentDriver, Zarinpal } from 'monopay'
+
+const driver = getPaymentDriver('zibal', {
+  merchantId: 'zibal',
+  // sandbox: true,
+})
 
 const corsHeaders = {
   'Access-Control-Allow-Origin': 'http://localhost:3001' || '*',
@@ -27,122 +25,100 @@ export async function POST(
   { params }: { params: { storeId: string } }
 ) {
   try {
-    const initParams: InitParams = {
-      merchantId: process.env.ZARINPAL_KEY!,
-      callbackURL: `${process.env.FRONTEND_URL}/cart`,
-      sandbox: true,
-    }
-    // const zarinpalV4 = Zarinpal.CreateInstance(initParams)
+    // console.log('paymentInfo', paymentInfo.url)
 
     const session = await getAuthSession()
     const userId = session?.user.id
 
-    // const users = prisma.user.findUnique({
-    //   where: {
-    //     id: Number(userId),
-    //   },
-    // })
+    const users = prisma.user.findUnique({
+      where: {
+        id: Number(userId),
+      },
+    })
 
     // What we get from frontend: `${process.env.NEXT_PUBLIC_API_URL}/checkout`,{productIds: items.map((item) => item.id),}
 
-    // const zarinpalPay = new ZarinpalPayment(
-    //   process.env.ZARINPAL_KEY!,
-    //   true,
-    //   true
-    // )
-    // const { productIds } = await req.json()
+    const { productIds, totalPrice } = await req.json()
 
-    // if (!productIds || productIds.length === 0) {
-    //   return new NextResponse('Product ids are required', { status: 400 })
-    // }
+    if (!productIds || productIds.length === 0) {
+      return new NextResponse('Product ids are required', { status: 400 })
+    }
 
-    // const products = await prisma.product.findMany({
-    //   where: {
-    //     id: {
-    //       in: productIds,
-    //     },
-    //   },
-    // })
+    const products = await prisma.product.findMany({
+      where: {
+        id: {
+          in: productIds,
+        },
+      },
+    })
 
-    // const order = await prisma.order.create({
-    //   data: {
-    //     storeId: params.storeId,
-    //     isPaid: false,
-    //     orderItems: {
-    //       create: productIds.map((productId: string) => ({
-    //         product: {
-    //           connect: {
-    //             id: productId,
-    //           },
-    //         },
-    //       })),
-    //     },
-    //   },
-    // })
+    const order = await prisma.order.create({
+      data: {
+        storeId: params.storeId,
+        isPaid: false,
+        orderItems: {
+          create: productIds.map((productId: string) => ({
+            product: {
+              connect: {
+                id: productId,
+              },
+            },
+          })),
+        },
+      },
+    })
 
-    // const line_items: ZarinpalType[] = []
-    // const list = products.forEach((product) => {
+    // const line_items: any = []
+
+    // products.forEach((product) => {
     //   line_items.push({
-    //     amount: product.price.toNumber(),
-    //     callback_url: `${process.env.FRONTEND_URL}/cart`,
-    //     // mobile: '',
-    //     // email: session?.user?.email || undefined,
-    //     // description: 'توضیحات تراکنش',
-    //     order_id: order.id,
+    //     quantity: 1,
+    //     price_data: {
+    //       // currency: 'USD',
+    //       product_data: {
+    //         name: product.name,
+    //       },
+    //       unit_amount: product.price.toNumber(),
+    //     },
     //   })
     // })
-    // const res = await zarinpalPay.create({
-    //   amount: 4500000,
+    console.log(totalPrice)
 
-    //   callback_url: `${process.env.FRONTEND_URL}/cart`,
-    //   // order_id: order.id,
-    //   description: 'payment',
-    // })
-
-    // console.log(res)
-
-    // const createpay = await zarinpal.create({
-    //   amount: 100000,
-    //   callback_url: `${process.env.FRONTEND_URL}/cart`,
-    //   mobile: '09352310831',
-    //   email: 'my@site.com',
-    //   description: 'توضیحات تراکنش',
-    //   // order_id: order.id,
-    // })
-
-    // const pay: PaymentRequest = {
-    //   amount: 1000,
-    //   callback_url: `${process.env.FRONTEND_URL}/cart`,
-    //   description: 'Test Payment',
-    //   currency: 'IRT',
-    //   metadata: {
-    //     email: 'a@b.com',
-    //     mobile: '09999999999',
-    //     order_id: '123',
-    //   } as PaymentMetadata,
-    // }
-    // const res = await zarinpalV4.requestPayment(pay)
-    // console.log(res)
-
-    // const response = await zarinpal.verifyPayment(res)
-
-    const driver = getPaymentDriver('zarinpal', {
-      merchantId: process.env.ZARINPAL_KEY!,
-      sandbox: true,
-    })
     const paymentInfo = await driver.requestPayment({
-      amount: 50000,
+      amount: +totalPrice,
+      referenceId: order.id,
       callbackUrl: `${process.env.FRONTEND_URL}/cart`,
       description: 'Description about the transaction',
     })
-    return NextResponse.json(
-      `<html>
-    <body>
-        <h1> We're redirecting you to the payment gateway... </h1>
-        <script>${paymentInfo.getScript()}</script>
-    </body>
-    </html>`
+
+    return NextResponse.json(paymentInfo, { headers: corsHeaders })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export async function GET(
+  req: Request,
+  res: Response,
+  { params }: { params: { storeId: string } }
+) {
+  try {
+    const { searchParams } = new URL(req.url)
+    // const categoryId = searchParams.get('categoryId') || undefined
+
+    const amount = searchParams.get('amount')
+    const referenceId = searchParams.get('referenceId')
+
+    console.log(amount, referenceId)
+    const receipt = await driver.verifyPayment(
+      {
+        amount: amount, // from database
+        referenceId, // from database
+      },
+      { searchParams, ...req.body }
     )
+    console.log(receipt)
+    return NextResponse.json(receipt)
   } catch (error) {
     console.log(error)
   }
--- lib/zarinpal.ts
+++
@@ -1,7 +0,0 @@
-import ZarinpalPayment from 'zarinpal-pay'
-// const zarinpal = new ZarinpalPayment(Merchant, isTomam, isSandbox)
-export const zarinpal = new ZarinpalPayment(
-  process.env.ZARINPAL_KEY!,
-  true,
-  true
-)
